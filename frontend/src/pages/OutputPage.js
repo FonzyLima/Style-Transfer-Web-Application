cdad2508545e44a6ac39b4c7addd1d6972f08ae5
leigharriane <81524834+leigharriane@users.noreply.github.com>
2022-06-29 05:10:28 +0800

    output page

--- frontend/src/pages/OutputPage.js
+++ frontend/src/pages/OutputPage.js
@@ -1,7 +1,47 @@
 import React from 'react'
+import '../styles/styles.scss';
 
 export default function OutputPage() {
   return (
-    <div>OutputPage</div>
+    <section className='outputsec'>
+      <div className='inputContainer'>
+        <a href='/Input' className='back'>
+          <svg width="30" height="30" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M27 16H5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M14 7L5 16L14 25" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          Back
+        </a>
+        <div className='inputComponents'>
+          <form>
+            <div className='left'>
+              <div className='step1'>
+                <div className='title'>Output</div>
+                <img className='outputimg'></img>
+                <button>Download Image</button>
+              </div>
+              <div className='step2'>
+                
+                
+              </div>
+            </div>
+            <div className='right'>
+              <div className='selected'>
+                <div className='title'>Image Selected</div>
+                <div className='selectedimg'>
+                  <img src=''></img>
+                </div>
+              </div>
+              <div className='style'>
+                <div className='title'>Style Image</div>
+                <div className='styleimg'>
+                  <img src=''></img>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
