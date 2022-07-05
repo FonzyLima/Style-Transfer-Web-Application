b8f8e74a791577c9da5d23161b4589c79b54aa45
tofypinpin <christopher_mari_pinpin@dlsu.edu.ph>
2022-07-05 15:53:40 +0800

    homejs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './styles/index.css';
-import Home from './pages/Home'
+import Home from './pages/Home.js'
 import Nav from './pages/Nav'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
